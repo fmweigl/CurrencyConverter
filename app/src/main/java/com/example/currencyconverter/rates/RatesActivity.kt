7eda1d5e7afa8b1b740d2bff9a20a027723b5fd4
Florian Weigl <florian.weigl@plan-d.com>
2020-06-27 16:52:55 +0200

    remove attribution activity, add attribution to flag creator in readme

--- app/src/main/java/com/example/currencyconverter/rates/RatesActivity.kt
+++ app/src/main/java/com/example/currencyconverter/rates/RatesActivity.kt
@@ -1,15 +1,11 @@
 package com.example.currencyconverter.rates
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.currencyconverter.R
 import com.example.currencyconverter.databinding.ActivityRatesBinding
-import com.example.currencyconverter.attribution.AttributionActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RatesActivity : AppCompatActivity() {
@@ -35,23 +31,5 @@ class RatesActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         viewModel.pauseUpdatingRates()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_rates, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_item_show_attribution) {
-            showAttributionActivity()
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun showAttributionActivity() {
-        startActivity(Intent(this, AttributionActivity::class.java))
     }
 }
